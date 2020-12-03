c1e68e1a42d34f0ed45dde6acf9500c1b0f46a8d
JosipHorvat <josiph988@gmail.com>
2020-12-03 14:35:46 +0100

    Quiz model.

--- app/src/main/java/com/horvat/quizapp/MainActivity.java
+++ app/src/main/java/com/horvat/quizapp/MainActivity.java
@@ -17,6 +17,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initViews();
+
+       QuizModel model = new QuizModel(R.string.q1,true);
+
     }
 
     private void initViews() {
