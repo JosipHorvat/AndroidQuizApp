b3bfb19baf3ec1e4661d6155ecec47c919c2ad29
JosipHorvat <josiph988@gmail.com>
2020-12-03 14:18:37 +0100

    Linear Layout design and strings.xml for English and Croatian language

--- app/src/main/java/com/horvat/quizapp/MainActivity.java
+++ app/src/main/java/com/horvat/quizapp/MainActivity.java
@@ -3,12 +3,36 @@ package com.horvat.quizapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnTrue, btnWrong;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
     }
+
+    private void initViews() {
+        btnTrue = findViewById(R.id.btnTrue);
+        btnWrong = findViewById(R.id.btnWrong);
+
+    }
+
+
+    public void onClickBtnTrue(View view){
+        Log.i("TAG", "Button true is tapped");
+    }
+
+   public void onClickBtnWrong(View view){
+        // context moze biti: this ili MainActivity.this.
+       Toast.makeText(getApplicationContext(), "Button wrong is clicked", Toast.LENGTH_SHORT).show();
+   }
+
 }
