d9b9677096bb6b0f95e658cd98a8cf0b8100ae05
JosipHorvat <josiph988@gmail.com>
2020-12-05 12:32:48 +0100

    Progress bar logic and userScore count.

--- app/src/main/java/com/horvat/quizapp/MainActivity.java
+++ app/src/main/java/com/horvat/quizapp/MainActivity.java
@@ -6,15 +6,19 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button btnTrue, btnWrong;
-    private TextView txtQuestion;
+    private TextView txtQuestion, txtQuizStats;
+    private ProgressBar quizPB;
     private int questionIndex;
     private int quizQuestion;
+
+    private int userScore;
 
     private QuizModel[] questionCollection = new QuizModel[]{
 
@@ -29,6 +33,8 @@ public class MainActivity extends AppCompatActivity {
             new QuizModel(R.string.q9, true),
             new QuizModel(R.string.q10, false)
     };
+
+    final int USER_PROGRESS = (int)Math.ceil(100.0/questionCollection.length);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,16 +54,22 @@ public class MainActivity extends AppCompatActivity {
         btnTrue = findViewById(R.id.btnTrue);
         btnWrong = findViewById(R.id.btnWrong);
         txtQuestion = findViewById(R.id.txtQuestion);
+        txtQuizStats = findViewById(R.id.txtQuizStats);
+        quizPB = findViewById(R.id.quizPB);
 
     }
 
 
     public void onClickBtnTrue(View view){
+        evaluateUsersAnswer(true);
         changeQuestionOnButtonTapped();
+
     }
 
    public void onClickBtnWrong(View view){
+       evaluateUsersAnswer(false);
        changeQuestionOnButtonTapped();
+
    }
 
    private void changeQuestionOnButtonTapped(){
@@ -70,6 +82,22 @@ public class MainActivity extends AppCompatActivity {
 
        quizQuestion = questionCollection[questionIndex].getmQuestion();
         txtQuestion.setText(quizQuestion);
-   }
 
+        quizPB.incrementProgressBy(USER_PROGRESS);
+        txtQuizStats.setText(userScore + "");
+   }
+        public void evaluateUsersAnswer(boolean userGuess){
+
+        boolean currentQuestionAnswer = questionCollection[questionIndex].ismAnswer();
+
+        if(currentQuestionAnswer == userGuess){
+            Toast.makeText(getApplicationContext(), R.string.correct_text, Toast.LENGTH_SHORT).show();
+
+            userScore = userScore +1;
+        }else{
+
+        }
+            Toast.makeText(getApplicationContext(), R.string.incorrect_text, Toast.LENGTH_SHORT).show();
+
+        }
 }
